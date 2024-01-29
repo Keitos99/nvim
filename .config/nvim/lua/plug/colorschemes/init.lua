0f6fa182baf13fc8508e658a55c513291a996641
Agsayan <agsayan0@gmail.com>
2024-01-29 20:01:15 +0100

    feat: added new colorschemes midnight and sonokai

--- .config/nvim/lua/plug/colorschemes/init.lua
+++ .config/nvim/lua/plug/colorschemes/init.lua
@@ -1,26 +1,17 @@
-function ColorMyPencils(color)
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
-
-  -- make background transparent
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 local kanagawa = require("plug.colorschemes.kanagawa")
 local rosepine = require("plug.colorschemes.rosepine")
 local modus = require("plug.colorschemes.modus")
 local sonokai = require("plug.colorschemes.sonokai")
+local midnight = require("plug.colorschemes.midnight")
 
 -- must be loaded before everything else
 sonokai.lazy = false
 sonokai.priority = 1000
 
-local M = {
+return {
+  midnight,
   sonokai,
   modus,
   kanagawa,
   rosepine,
 }
-
-return M
--- .config/nvim/lua/plug/colorschemes/sonokai.lua
+++ .config/nvim/lua/plug/colorschemes/sonokai.lua
@@ -1,6 +1,7 @@
 return {
   "sainnhe/sonokai",
   config = function()
+    vim.g.sonokai_style = "atlantis"
     vim.cmd.colorscheme({ "sonokai" })
   end,
 }
