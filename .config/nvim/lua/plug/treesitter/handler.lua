05521f11d675f0edd757d70ba796a91cb194a95b
Agsayan <agsayan0@gmail.com>
2024-05-27 22:33:58 +0200

    replaced print with a notify

--- .config/nvim/lua/plug/treesitter/handler.lua
+++ .config/nvim/lua/plug/treesitter/handler.lua
@@ -29,7 +29,7 @@ M.disable_large = function(lang, buf)
   local max_filesize = 300 * 1024 -- 300 KB
   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
   if ok and stats and stats.size > max_filesize then
-    print("Disabled for " .. tostring(lang) .. ": " .. stats.size)
+    vim.notify_once("Disabled for " .. tostring(lang) .. ": " .. stats.size)
     return false
   end
 end
