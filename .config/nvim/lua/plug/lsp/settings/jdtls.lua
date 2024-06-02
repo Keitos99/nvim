ed016b973fcc07d6303b925ce1fc2ee94134b636
Agsayan <agsayan0@gmail.com>
2024-06-02 19:14:50 +0200

    refactored

--- .config/nvim/lua/plug/lsp/settings/jdtls.lua
+++ .config/nvim/lua/plug/lsp/settings/jdtls.lua
@@ -1,16 +1,23 @@
+local jdtls = require("jdtls")
+local lsp = require("plug.lsp.handlers")
+local global = require("config.globals").jdtls
+
 local HOME = os.getenv("HOME")
 local XDG_DATA_HOME = os.getenv("XDG_DATA_HOME")
+
 local LSP_SERVER = HOME .. "/dev/eclipse/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/"
 local JAVA = HOME .. "/.local/jdks/jdk-17.0.2+8/bin/java" -- NOTE: must be the same as the one to compile jdtls and jdtls-dap
 local WORKSPACE_PATH = XDG_DATA_HOME .. "/jdtls-workspace/"
 local CONFIG = "linux"
 
-local jdtls = require("jdtls")
-local lsp = require("plug.lsp.handlers")
-local global = require("config.globals").jdtls
+-- set the capabilities
+local capabilities = lsp.capabilities
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+capabilities = vim.tbl_deep_extend("force", extendedClientCapabilities, capabilities)
 
--- jar patterns to search for
-local jar_patterns = {
+local ROOT_MARKERS = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", "src" }
+local JAR_PATTERNS = {
   "/dev/microsoft/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
   "/dev/dgileadi/vscode-java-decompiler/server/*.jar",
   "/dev/microsoft/vscode-java-test/java-extension/com.microsoft.java.test.plugin/target/*.jar",
@@ -18,25 +25,17 @@ local jar_patterns = {
   "/dev/microsoft/vscode-java-test/java-extension/com.microsoft.java.test.runner/lib/*.jar",
 }
 
--- Find root of project
-local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", "src" }
-local root_dir = require("jdtls.setup").find_root(root_markers)
-if root_dir == "" or root_dir == nil then
-  return
-end
+-- Find the root of the project
+local ROOT_DIR = require("jdtls.setup").find_root(ROOT_MARKERS)
+assert(ROOT_DIR ~= nil and ROOT_DIR ~= "", "Root directory can not be determined!")
 
-local PROJECT_NAME = vim.fn.fnamemodify(root_dir, ":p:h:t")
+local PROJECT_NAME = vim.fn.fnamemodify(ROOT_DIR, ":p:h:t")
 local WORKSPACE_DIR = WORKSPACE_PATH .. PROJECT_NAME
 
-local capabilities = lsp.capabilities
-local extendedClientCapabilities = jdtls.extendedClientCapabilities
-extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
-capabilities = vim.tbl_deep_extend("force", extendedClientCapabilities, capabilities)
-
+-- npm install broke: https://github.com/npm/cli/issues/2508
+-- So gather the required jars manually; this is based on the gulpfile.js in the vscode-java-test repo
+-- stolen from mfussnegger
 local function get_bundles(jar_patterns)
-  -- npm install broke: https://github.com/npm/cli/issues/2508
-  -- So gather the required jars manually; this is based on the gulpfile.js in the vscode-java-test repo
-  -- stolen from mfussnegger
   local plugin_path =
     "/dev/microsoft/vscode-java-test/java-extension/com.microsoft.java.test.plugin.site/target/repository/plugins/"
   local bundle_list = vim.tbl_map(function(x)
@@ -68,24 +67,36 @@ local function get_bundles(jar_patterns)
   return bundles
 end
 
-local function get_libs(project_root)
-  local find_jar_libs_cmd = "find " .. project_root .. "/libs -type f | grep jar"
+local function read_libs(project_root)
+  if not (vim.fn.filereadable(project_root .. "/.classpath") == 1) then
+    local find_jar_libs_cmd = "find " .. project_root .. "/libs -type f | grep jar"
+    return require("config.helper").cmd_to_table(find_jar_libs_cmd)
+  end
+
+  -- There is .classpath file
+  local number_of_non_existent_jars = 0
   local extract_classpathentries_cmd = "grep 'classpathentry kind=\"lib\"' "
     .. project_root
     .. "/.classpath | sed 's/.*classpathentry kind=\"lib\" path=\"//' | sed 's/\".*//'"
+    print(extract_classpathentries_cmd)
+  local jars = require("config.helper").cmd_to_table(extract_classpathentries_cmd)
 
-  if vim.fn.filereadable(project_root .. "/.classpath") == 1 then
-    local libs = require("config.helper").cmd_to_table(extract_classpathentries_cmd)
-
-    local new_table = {}
-    for _, lib in ipairs(libs) do
-      table.insert(new_table, project_root .. "/" .. lib)
+  local new_table = {}
+  for _, jar_file in ipairs(jars) do
+    jar_file = project_root .. "/" .. jar_file
+    if vim.fn.filereadable(jar_file) == 1 then
+      table.insert(new_table, jar_file)
+    else
+      number_of_non_existent_jars = number_of_non_existent_jars + 1
     end
-    return new_table
   end
 
-  return require("config.helper").cmd_to_table(find_jar_libs_cmd)
+  if number_of_non_existent_jars > 0 then
+    vim.notify(string.format("There are %s entries in the .classpath file, that do not exist!", number_of_non_existent_jars))
+  end
+  return new_table
 end
+
 
 return {
   cmd = {
@@ -112,9 +123,7 @@ return {
     WORKSPACE_DIR,
   },
   capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    -- lsp.on_attach(client, bufnr)
-
+  on_attach = function(_, bufnr)
     jdtls.setup_dap({
       hotcodereplace = "auto",
       config_overrides = {
@@ -134,7 +143,7 @@ return {
 
     require("jdtls.dap").setup_dap_main_class_configs() -- needs to be called to activate the dap function
   end,
-  root_dir = root_dir,
+  root_dir = ROOT_DIR,
   -- NOTE: look here for a list of options:
   -- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
   settings = {
@@ -174,7 +183,7 @@ return {
       },
       project = {
         -- import all jar files under the libs directory
-        referencedLibraries = get_libs(root_dir),
+        referencedLibraries = read_libs(ROOT_DIR),
       },
       referenceCodeLens = {
         enabled = true,
@@ -203,6 +212,6 @@ return {
     },
   },
   init_options = {
-    bundles = get_bundles(jar_patterns),
+    bundles = get_bundles(JAR_PATTERNS),
   },
 }
