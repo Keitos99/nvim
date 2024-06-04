3c841aa35b25cbd9921b20ec74f0b148d927f8f4
Agsayan <agsayan0@gmail.com>
2024-06-04 20:24:41 +0200

    fix: typo

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -1,5 +1,4 @@
--- MUST be called from after/ftplugin/java.lua, because it has to called for each java file
--- or the lsp will not be attached
+-- MUST be called from after/ftplugin/java.lua, because only then will jdtls attach to each java file
 local jdtls = require("jdtls")
 local config = require("config.lsp.settings.jdtls")
 jdtls.start_or_attach(config)
