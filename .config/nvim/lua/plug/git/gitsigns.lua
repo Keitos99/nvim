8d7dd5957b18ae583a9f105b8fea088c93d06b34
Agsayan <agsayan0@gmail.com>
2024-08-03 23:12:58 +0200

    added descriptions to gitsign keymaps

--- .config/nvim/lua/plug/git/gitsigns.lua
+++ .config/nvim/lua/plug/git/gitsigns.lua
@@ -29,7 +29,7 @@ return {
       local function map(mode, l, r, opts)
         opts = opts or {}
         opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, { buffer = bufnr })
+        vim.keymap.set(mode, l, r, opts)
       end
 
       -- Navigation
@@ -46,17 +46,27 @@ return {
       end, { expr = true })
 
       -- Actions
-      map("n", "<leader>gs", gs.stage_hunk)
-      map("n", "<leader>gu", gs.undo_stage_hunk)
-      map("n", "<leader>gr", gs.reset_hunk)
-      map("v", "<leader>gs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
-      map("v", "<leader>gr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
-      map("n", "<leader>gS", gs.stage_buffer)
-      map("n", "<leader>gR", gs.reset_buffer)
-      map("n", "<leader>gp", gs.preview_hunk)
-      map("n", "<leader>gB", gs.toggle_current_line_blame)
-      map("n", "<leader>gbb", function() gs.blame_line({ full = true }) end)
-      map("n", "<leader>gd", gs.toggle_deleted)
+      map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage hunk" })
+      map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+      map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+      map(
+        "v",
+        "<leader>gs",
+        function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+        { desc = "Stage slected hunk" }
+      )
+      map(
+        "v",
+        "<leader>gr",
+        function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+        { desc = "Reset selected hunk" }
+      )
+      map("n", "<leader>gS", gs.stage_buffer, { desc = "Stage buffer" })
+      map("n", "<leader>gR", gs.reset_buffer, { desc = "Reset buffer" })
+      map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
+      map("n", "<leader>gB", gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
+      map("n", "<leader>gbb", function() gs.blame_line({ full = true }) end, { desc = "Blame line" })
+      map("n", "<leader>gd", gs.toggle_deleted, { desc = "Toggle deleted" })
 
       -- Text object
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
