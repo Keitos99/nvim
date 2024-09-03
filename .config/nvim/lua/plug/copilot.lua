3a6142ffc993f23e133f18c13590cce339c9db65
Agsayan <agsayan0@gmail.com>
2024-09-03 20:53:42 +0200

    feat(plugin): Add Plugin gp.nvim for interacting with Github Copilot
    Allows developers to communicate with GitHub Copilot within the editor.

--- .config/nvim/lua/plug/copilot.lua
+++ .config/nvim/lua/plug/copilot.lua
@@ -1,11 +1,35 @@
 return {
-  "github/copilot.vim",
-  lazy = false,
-  init = function()
-    vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-    })
-    vim.g.copilot_no_tab_map = true
-  end,
+  {
+    "github/copilot.vim",
+    lazy = false,
+    init = function()
+      vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+  {
+    "robitx/gp.nvim",
+    lazy = false,
+    config = function()
+      local conf = {
+        -- For customization, refer to Install > Configuration in the Documentation/Readme
+        providers = {
+          copilot = {
+            endpoint = "https://api.githubcopilot.com/chat/completions",
+            secret = {
+              "bash",
+              "-c",
+              "cat ~/.config/github-copilot/apps.json | sed -e 's/.*oauth_token...//;s/\".*//'",
+            },
+          },
+        },
+      }
+      require("gp").setup(conf)
+
+      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+    end,
+  },
 }
