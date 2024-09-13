395fe49116d4a9f0c6445241fdaba5eafbcd12fb
Agsayan <agsayan0@gmail.com>
2024-09-13 15:46:18 +0200

    feat(typescript): added nvim-dap configurations for typescript

--- .config/nvim/lua/plug/typescript.lua
+++ .config/nvim/lua/plug/typescript.lua
@@ -1,6 +1,11 @@
 return {
-  "pmizio/typescript-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = true,
-  lazy = false,
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = true,
+    lazy = false,
+  },
+  {
+    "mxsdev/nvim-dap-vscode-js",
+  },
 }
