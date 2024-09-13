2ba9d70c3752c4428f2da6363dbe915dda63c444
Agsayan <agsayan0@gmail.com>
2024-09-13 15:47:34 +0200

    fix(obsidian): Lazy loading with ObsidianSearch Command

--- .config/nvim/lua/plug/obsidian.lua
+++ .config/nvim/lua/plug/obsidian.lua
@@ -5,6 +5,7 @@ return {
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   cmd = {
+    "ObsidianSearch",
     "ObsidianYesterday",
     "ObsidianToday",
     "ObsidianTomorrow",
