f67094aa068c662ce027ac7df6c5ed4105bc2543
Agsayan <agsayan0@gmail.com>
2024-09-22 21:35:29 +0200

    feat(colorscheme): catppuccin added

--- .config/nvim/lua/plug/colorschemes/init.lua
+++ .config/nvim/lua/plug/colorschemes/init.lua
@@ -2,14 +2,16 @@ local kanagawa = require("plug.colorschemes.kanagawa")
 local modus = require("plug.colorschemes.modus")
 local sonokai = require("plug.colorschemes.sonokai")
 local midnight = require("plug.colorschemes.midnight")
+local catppuccin = require("plug.colorschemes.catppuccin")
 
 -- must be loaded before everything else
-sonokai.lazy = false
-sonokai.priority = 1000
+kanagawa.lazy = false
+kanagawa.priority = 1000
 
 return {
   midnight,
   sonokai,
   modus,
   kanagawa,
+  catppuccin,
 }
