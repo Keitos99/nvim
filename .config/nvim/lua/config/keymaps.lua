1de1560d07aba036460bcbad43261f146ff6de9a
Agsayan <agsayan0@gmail.com>
2024-10-06 00:12:36 +0200

    fix: improve window resizing behavior for C-h/j/k/l mappings
    - Resizing is now based on the bottom and left edges of the window, making it more intuitive.
    - Handles cases where the window is in the middle, resizing as if it were on the edge.
    - Replaced the smart-splits plugin with the resize_window function

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -24,6 +24,49 @@ local map = function(mode, key, result, options)
   vim.keymap.set(mode, key, result, opts)
 end
 
+-- this function is based on the plugin mrjones2014/smart-splits.nvim
+local function resize_window(direction, amount)
+  ---@param direction string
+  ---@return string
+  local function get_opposite_direction(direction)
+    if direction == "h" then return "l" end
+    if direction == "l" then return "h" end
+    if direction == "j" then return "k" end
+    if direction == "k" then return "j" end
+
+    assert(false, "Invalid direction " .. direction)
+    return ""
+  end
+
+  ---@param amount integer
+  ---@return string
+  local function format_amount(amount)
+    if amount > 0 then
+      return "+" .. amount
+    else
+      -- the minus sign will automatically be added
+      return tostring(amount)
+    end
+  end
+
+  local winnr = vim.fn.winnr()
+  local is_at_edge = vim.fn.winnr(direction) == winnr
+  local is_opposite_edge = vim.fn.winnr(get_opposite_direction(direction)) == winnr
+
+  local is_horizontal_resize = direction == "h" or direction == "l"
+  local resize_cmd = is_horizontal_resize and "vertical resize" or "resize"
+  if is_at_edge then
+    -- resize the window in the given direction
+    vim.cmd(resize_cmd .. " " .. format_amount(amount))
+  elseif is_opposite_edge then
+    -- resize the window in the opposite direction
+    vim.cmd(resize_cmd .. " " .. format_amount(-1 * amount))
+  else
+    -- is somewhere in the middle, so resize the window in the given direction
+    vim.cmd(resize_cmd .. " " .. format_amount(amount))
+  end
+end
+
 map("", "<Space>", "<Nop>")
 
 -- moving between windows NOTE: will be overwritten by vim-tmux-navigator
@@ -32,11 +75,10 @@ map("n", "<C-j>", "<C-w>j", { desc = "Move to the window below" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to the window above" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to the window to the right" })
 
--- resize windows wit description
-map("n", "<C-Up>", ":resize +2<CR>", { desc = "Resize the window up" })
-map("n", "<C-Down>", ":resize -2<CR>", { desc = "Resize the window down" })
-map("n", "<C-Left>", ":vertical resize +2<CR>", { desc = "Resize the window to the left" })
-map("n", "<C-Right>", ":vertical resize -2<CR>", { desc = "Resize the window to the right" })
+map("n", "<C-Up>", function() resize_window("j", 2) end, { desc = "Increase the window height" })
+map("n", "<C-Down>", function() resize_window("j", -2) end, { desc = "Decrease the window height" })
+map("n", "<C-Right>", function() resize_window("h", 2) end, { desc = "Increase the window width" })
+map("n", "<C-Left>", function() resize_window("h", -2) end, { desc = "Decrease the window width" })
 
 -- buffers
 map("n", "<S-q>", ":bdelete<CR>", { desc = "Close the current buffer" })
