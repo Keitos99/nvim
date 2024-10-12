091b9f4ce8d4b47b54638dbc2ea7327595b60564
Agsayan <agsayan0@gmail.com>
2024-10-12 10:13:12 +0200

    fix: allow opening java files wihout a project root
    - and plugins were updated (Lazy update)

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -1,4 +1,10 @@
 -- MUST be called from after/ftplugin/java.lua, because only then will jdtls attach to each java file
 local jdtls = require("jdtls")
-local config = require("config.lsp.settings.jdtls")
+local was_successful, config = pcall(require, "config.lsp.settings.jdtls")
+
+if not was_successful then
+  -- print the error message
+  print("Error loading jdtls config: ", config)
+  return
+end
 jdtls.start_or_attach(config)
