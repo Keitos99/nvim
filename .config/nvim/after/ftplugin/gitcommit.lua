96561a22e275be5807c0b0a396f3f5ac2d240544
Agsayan <agsayan0@gmail.com>
2024-11-01 00:17:35 +0100

    feat(gitcommit): spelling is active, while writing commit messages
    - increased textwidth to 80

---
+++ .config/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,9 @@
+-- activate spelling
+vim.api.nvim_command("setlocal spell")
+
+-- Enable line wrapping
+vim.wo.wrap = true
+
+-- Set the width of the text to 80 characters
+vim.wo.colorcolumn = "80"
+vim.bo.textwidth = 80
