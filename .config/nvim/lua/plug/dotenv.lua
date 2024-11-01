4276e1aa8e7de49abc0a35a957ebf50394124145
Agsayan <agsayan0@gmail.com>
2024-11-01 13:49:56 +0100

    feat: auto-reload environment variables on .env save

--- .config/nvim/lua/plug/dotenv.lua
+++ .config/nvim/lua/plug/dotenv.lua
@@ -6,4 +6,14 @@ return {
     enable_on_load = true, -- will load your .env file upon loading a buffer
     verbose = false, -- show error notification if .env file is not found and if .env is loaded
   },
+
+  config = function(_, opts)
+    require("dotenv").setup(opts)
+
+    -- Reload environment variables when .env file is saved
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = ".env",
+      callback = function() require("dotenv").command() end,
+    })
+  end,
 }
