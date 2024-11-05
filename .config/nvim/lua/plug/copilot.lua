6ba2e63244016ae01efad3ea10b4120c760b47a1
Agsayan <agsayan0@gmail.com>
2024-11-05 15:42:16 +0100

    Added/modified keymaps for easier access to CopilotChat features

--- .config/nvim/lua/plug/copilot.lua
+++ .config/nvim/lua/plug/copilot.lua
@@ -29,8 +29,67 @@ return {
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      mappings = {
+        -- Reset the chat buffer
+        reset = {
+          normal = "<C-x>",
+          insert = "<C-x>",
+        },
+        -- Submit the prompt to Copilot
+        submit_prompt = {
+          normal = "<CR>",
+          insert = "<C-CR>",
+        },
+        -- Accept the diff
+        accept_diff = {
+          normal = "<C-y>",
+          insert = "<C-y>",
+        },
+        -- Yank the diff in the response to register
+        yank_diff = {
+          normal = "gmy",
+        },
+        -- Show the diff
+        show_diff = {
+          normal = "gmd",
+        },
+        -- Show the prompt
+        show_system_prompt = {
+          normal = "gmp",
+        },
+        -- Show the user selection
+        show_user_selection = {
+          normal = "gms",
+        },
+        -- Show help
+        show_help = {
+          normal = "gmh",
+        },
+      },
     },
-    -- See Commands section for default commands if you want to lazy load on them
+    config = function(_, opts)
+      local chat = require("CopilotChat")
+      chat.setup(opts)
+
+      -- Setup the CMP integration
+      require("CopilotChat.integrations.cmp").setup()
+    end,
+    keys = {
+      { "<leader>aa", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
+      { "<leader>al", "<cmd>CopilotChatReset<cr>", desc = "CopilotChat - Clear buffer and chat history" },
+      { "<leader>af", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
+      { mode = { "n", "v", "x" }, "<leader>ar", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
+      {
+        "<leader>aq",
+        function()
+          vim.ui.input({ prompt = "Ask Copilot: " }, function(input)
+            if input == "" then return end
+
+            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+          end)
+        end,
+        desc = "CopilotChat - Quick chat",
+      },
+    },
   },
 }
