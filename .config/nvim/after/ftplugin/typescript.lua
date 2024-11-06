d20d7be440cafece25f71bf3cf86a42b068bc667
Agsayan <agsayan0@gmail.com>
2024-11-06 22:09:35 +0100

    refactor: simplified the setting up the server
    - variable for basic launch config added

--- .config/nvim/after/ftplugin/typescript.lua
+++ .config/nvim/after/ftplugin/typescript.lua
@@ -1,63 +1,53 @@
 local dap = require("dap")
 local dap_utils = require("dap.utils")
-local filename = vim.api.nvim_buf_get_name(0)
+local mason_registry = require("mason-registry")
+local js_debug_package = mason_registry.get_package("js-debug-adapter")
+local js_debug_path = js_debug_package:get_install_path() .. "/js-debug-adapter"
 
-dap.adapters["pwa-node"] = {
+local defaulf_adapter_config = {
   type = "server",
   host = "127.0.0.1",
   port = 8123,
   executable = {
-    command = "/home/agsayan/.local/share/nvim/mason/packages/js-debug-adapter/js-debug-adapter",
+    command = js_debug_path,
   },
 }
 
-dap.adapters["node"] = {
-  type = "server",
-  host = "127.0.0.1",
-  port = 8123,
-  executable = {
-    command = "/home/agsayan/.local/share/nvim/mason/packages/js-debug-adapter/js-debug-adapter",
-  },
+local base_config = {
+  skipFiles = { "<node_internals>/**" },
+  console = "integratedTerminal",
+  outputCapture = "std",
+  cwd = "${workspaceFolder}",
 }
+
+dap.adapters["pwa-node"] = defaulf_adapter_config
+dap.adapters["node"] = defaulf_adapter_config
 
 local js_based_languages = { "javascript", "typescript" }
 for _, language in ipairs(js_based_languages) do
   dap.configurations[language] = {
-
-    {
-      -- Configuration for R4
+    vim.tbl_extend("force", base_config, {
       type = "pwa-node",
       request = "launch",
       name = "Launch file (ts-node-dev)",
-      skipFiles = { "<node_internals>/**" },
       runtimeExecutable = "${workspaceFolder}/node_modules/.bin/ts-node-dev",
       runtimeArgs = { "--respawn" },
       program = "${file}",
-      console = "integratedTerminal", -- Use integrated terminal for better visibility
-      outputCapture = "std", -- Capture standard output for debugging
-      cwd = "${workspaceFolder}",
-    },
-    {
+    }),
+    vim.tbl_extend("force", base_config, {
       type = "pwa-node",
       request = "launch",
       name = "Launch file",
-      skipFiles = { "<node_internals>/**" },
       runtimeExecutable = "node",
       runtimeArgs = { "--inspect" },
       program = "${file}",
-      console = "integratedTerminal", -- Use integrated terminal for better visibility
-      outputCapture = "std", -- Capture standard output for debugging
-      cwd = "${workspaceFolder}",
-    },
-    {
+    }),
+    vim.tbl_extend("force", base_config, {
       type = "pwa-node",
       request = "attach",
       name = "Attach process",
       program = "${file}",
       processId = dap_utils.pick_process,
-      cwd = "${workspaceFolder}",
-      console = "integratedTerminal", -- Use integrated terminal for better visibility
-      outputCapture = "std", -- Capture standard output for debugging
-    },
+    }),
   }
 end
