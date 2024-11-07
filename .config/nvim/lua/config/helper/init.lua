a4651d231c0743a0a128f3519a51873489410db0
Agsayan <agsayan0@gmail.com>
2024-11-07 21:29:21 +0100

    refactor: deprecated functions and APIs replaced with alternatives

--- .config/nvim/lua/config/helper/init.lua
+++ .config/nvim/lua/config/helper/init.lua
@@ -1,8 +1,6 @@
 local M = {}
 
-function M.is_existing_dir(path)
-  return vim.fn.isdirectory(path) == 1
-end
+function M.is_existing_dir(path) return vim.fn.isdirectory(path) == 1 end
 
 ---@return number: number of listed/open buffers
 function M.get_number_of_buffers()
@@ -28,28 +26,20 @@ function M.sync_job(cmd, args)
 end
 
 function M.has_value(table, element)
-  if type(table) ~= "table" then
-    return false
-  end
+  if type(table) ~= "table" then return false end
 
   for _, value in pairs(table) do
-    if value == element then
-      return true
-    end
+    if value == element then return true end
   end
   return false
 end
 
-function M.is_empty(table)
-  return next(table) == nil
-end
+function M.is_empty(table) return next(table) == nil end
 
 function M.cmd_to_table(cmd)
   local temp_table = {}
   local pipe = io.popen(cmd)
-  if pipe == nil then
-    return temp_table
-  end
+  if pipe == nil then return temp_table end
 
   for line in pipe:lines() do
     table.insert(temp_table, line)
@@ -67,16 +57,14 @@ function M.find_root(fname)
   local root_patterns = require("config.globals").root_patterns
 
   if path ~= "" then
-    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+    for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
       local workspace = client.config.workspace_folders
-      local paths = workspace and vim.tbl_map(function(ws)
-        return vim.uri_to_fname(ws.uri)
-      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+      local paths = workspace and vim.tbl_map(function(ws) return vim.uri_to_fname(ws.uri) end, workspace)
+        or client.config.root_dir and { client.config.root_dir }
+        or {}
       for _, p in ipairs(paths) do
-        local r = vim.loop.fs_realpath(p)
-        if path:find(r, 1, true) then
-          roots[#roots + 1] = r
-        end
+        local r = vim.uv.fs_realpath(p)
+        if path:find(r, 1, true) then roots[#roots + 1] = r end
       end
     end
   end
@@ -91,9 +79,7 @@ function M.find_root(fname)
       root = vim.fs.find(root_patterns, { path = path, upward = true })[1]
       path = vim.fs.dirname(path)
 
-      if root ~= nil or path == "/" then
-        should_run = false
-      end
+      if root ~= nil or path == "/" then should_run = false end
     end
     root = root and vim.fs.dirname(root) or vim.loop.cwd()
   end
@@ -114,8 +100,6 @@ function M.get_module(fname)
   return ""
 end
 
-function M.has(plugin)
-  return require("plug")[plugin] ~= nil
-end
+function M.has(plugin) return require("plug")[plugin] ~= nil end
 
 return M
