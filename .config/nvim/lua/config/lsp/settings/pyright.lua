241b982b79dbdc96a9925083b0ab3954198d1962
Agsayan <agsayan0@gmail.com>
2024-12-07 14:09:48 +0100

    exclude build directory from processing and activate type checking

--- .config/nvim/lua/config/lsp/settings/pyright.lua
+++ .config/nvim/lua/config/lsp/settings/pyright.lua
@@ -11,7 +11,7 @@ return {
   settings = {
     python = {
       analysis = {
-        typeCheckingMode = "off", -- TODO: check if it should be true
+        typeCheckingMode = "basic", -- TODO: check if it should be true
         diagnosticSeverityOverrides = {
           reportUnusedImport = "warning",
         },
@@ -20,6 +20,8 @@ return {
       -- or add a pyrightconfig.json to the project that tells where the project venvs are
       -- NOTE: https://www.reddit.com/r/neovim/comments/18kyb5s/comment/kdvf597/?utm_source=share&utm_medium=web2x&context=3
       pythonPath = helper.get_python_binary(vim.api.nvim_buf_get_name(0)),
+
+      exclude = { "build", "**/node_modules", "**/__pycache__", "src/experimental", "src/typestubs" },
     },
   },
 }
