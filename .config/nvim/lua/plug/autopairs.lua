7253236c524b3622f8d3755b4aca01be41bcd6ae
Agsayan <agsayan0@gmail.com>
2024-12-21 14:58:30 +0100

    configure autopairs to use default settings

--- .config/nvim/lua/plug/autopairs.lua
+++ .config/nvim/lua/plug/autopairs.lua
@@ -6,21 +6,10 @@ local M = {
     ts_config = {
       lua = { "string", "source" },
       javascript = { "string", "template_string" },
-      java = false,
+      java = { "string" },
     },
-    disable_filetype = { "TelescopePrompt", "spectre_panel" },
     fast_wrap = {
-      map = "<A-e>",
-      chars = { "{", "[", "(", '"', "'" },
-      pattern = [=[[%'%"%>%]%)%}%,]]=],
-      end_key = "$",
-      before_key = "h",
-      after_key = "l",
-      cursor_pos_before = true,
-      keys = "qwertyuiopzxcvbnmasdfghjkl",
-      manual_position = true,
-      highlight = "Search",
-      highlight_grey = "Comment",
+      map = "<A-e>", -- documenting default key mapping
     },
   },
 }
@@ -32,10 +21,8 @@ function M.config(_, opts)
   -- If you want insert `(` after select function or method item
   local cmp_autopairs = require("nvim-autopairs.completion.cmp")
   local cmp_status_ok, cmp = pcall(require, "cmp")
-  if not cmp_status_ok then
-    return
-  end
-  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ filetypes = { tex = false, ["4gl"] = false } }))
+  if not cmp_status_ok then return end
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 return M
