a4198fb8edd840313c6355e59666e855832a9812
Agsayan <agsayan0@gmail.com>
2025-04-06 19:41:11 +0200

    refactor: move hlsearch toggle logic to autocmds.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,21 +4,3 @@ require("config.keymaps")
 require("config.lazy") -- load my plugins
 require("config.redir")
 require("config.autocmds")
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    -- disable hlsearch automatically when your search is done. Enable it, when pressing one of the defined keys
-    local ns = vim.api.nvim_create_namespace("toggle_hlsearch")
-
-    local function toggle_hlsearch(char)
-      if vim.fn.mode() ~= "n" then return end
-      local keys = { "<CR>", "n", "N", "*", "#", "?", "/" }
-      local new_hlsearch = vim.tbl_contains(keys, vim.fn.keytrans(char))
-
-      if vim.opt.hlsearch ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
-    end
-
-    vim.on_key(toggle_hlsearch, ns)
-  end,
-})
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -63,3 +63,21 @@ autocmd("LspAttach", {
     lsp.on_attach(client, bufnr)
   end,
 })
+
+-- disable hlsearch automatically when your search is done. Enable it, when pressing one of the defined keys
+autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    local ns = vim.api.nvim_create_namespace("toggle_hlsearch")
+
+    local function toggle_hlsearch(char)
+      if vim.fn.mode() ~= "n" then return end
+      local keys = { "<CR>", "n", "N", "*", "#", "?", "/" }
+      local new_hlsearch = vim.tbl_contains(keys, vim.fn.keytrans(char))
+
+      if vim.opt.hlsearch ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
+    end
+
+    vim.on_key(toggle_hlsearch, ns)
+  end,
+})
