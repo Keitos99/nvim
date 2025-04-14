77f49904ab09688d11759baa88dfbcc31de2d9f7
Agsayan <agsayan0@gmail.com>
2025-04-14 21:45:48 +0200

    feat: ts-error-translator added

--- .config/nvim/lua/plug/typescript.lua
+++ .config/nvim/lua/plug/typescript.lua
@@ -5,4 +5,10 @@ return {
     opts = true,
     ft = { "typescript", "javascript" },
   },
+
+  {
+    "dmmulroy/ts-error-translator.nvim",
+    ft = { "typescript", "javascript" },
+    config = function() require("ts-error-translator").setup() end,
+  },
 }
