17398a27fc30fd3494f5058919df941511e1368e
Agsayan <agsayan0@gmail.com>
2025-04-17 17:57:19 +0200

    refactor: moved to the native LSP API

--- .config/nvim/lua/config/lsp/mason.lua
+++ .config/nvim/lua/config/lsp/mason.lua
@@ -35,14 +35,14 @@ function M.setup_lsp(mason_lsp_config)
   for _, server_name in ipairs(mason_lsp_config.get_installed_servers()) do
     local can_be_setup = not vim.tbl_contains(do_not_auto_setup, server_name)
     if can_be_setup then
-      local opts = {}
       local has_settings, server_opts = pcall(require, "config.lsp.settings." .. server_name)
 
       if has_settings then
         -- extend with lsp specified configuration
-        opts = vim.tbl_extend("force", lspconfig.util.default_config, server_opts)
+        vim.lsp.config(server_name, server_opts)
       end
-      lspconfig[server_name].setup(opts)
+
+      vim.lsp.enable(server_name)
     end
   end
 end
