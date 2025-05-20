3fcef12d75f8315bee328a4ef329168a764cbb69
Agsayan <agsayan0@gmail.com>
2025-05-20 16:08:39 +0200

    refactor: auto enable the LSPs using mason

--- .config/nvim/lua/plug/lsp/mason.lua
+++ .config/nvim/lua/plug/lsp/mason.lua
@@ -8,19 +8,45 @@ local M = {
   cmd = "Mason",
 }
 
+function installNonLSPTools(tools)
+  local mr = require("mason-registry")
+  for _, tool in ipairs(tools) do
+    local p = mr.get_package(tool)
+    if not p:is_installed() then p:install() end
+  end
+end
+
 M.config = function()
   local mason = require("mason")
   local mason_lspconfig = require("mason-lspconfig")
-  local mason_install_dir = require("config.globals").mason.install_dir
+  local mason_config = require("config.globals").mason
+
+  local mason_install_dir = mason_config.install_dir
+  local tools = mason_config.tools
+  local lsps = mason_config.lsps
 
   mason.setup({
     install_root_dir = mason_install_dir,
     ui = { border = "rounded" },
   })
 
-  local mason_handler = require("config.lsp.mason")
-  mason_handler.install_tools(mason_lspconfig)
-  mason_handler.setup_lsp(mason_lspconfig)
+  -- auto-install not-lsp tools
+  installNonLSPTools(tools)
+
+  -- auto-install formatters, lsps, etc.
+  mason_lspconfig.setup({
+    ensure_installed = lsps,
+    automatic_installation = true,
+    automatic_enable = {
+      exclude = {
+        "tsserver",
+        "ts_ls",
+        "rust_analyzer",
+      },
+    },
+  })
+
+  vim.lsp.enable("jdtls")
 end
 
 return M
