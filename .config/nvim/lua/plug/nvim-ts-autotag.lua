41edb1e568456ffe80653d4f7f23d89bb1c52478
Agsayan <agsayan0@gmail.com>
2025-07-25 20:00:24 +0200

    refactor: nvim-ts-autotag moved to web-dev

--- .config/nvim/lua/plug/nvim-ts-autotag.lua
+++
@@ -1,6 +0,0 @@
--- Autoclosing tags for HTML and JSX.
-return {
-  "windwp/nvim-ts-autotag",
-  lazy = false,
-  opts = {},
-}
--- .config/nvim/lua/plug/web-dev.lua
+++ .config/nvim/lua/plug/web-dev.lua
@@ -1,25 +1,33 @@
--- tailwind-tools.lua
----@type LazyPluginSpec
+local web_filetypes = {
+  "html",
+  "svelte",
+  "css",
+  "javascript",
+  "typescript",
+  "javascriptreact",
+  "typescriptreact",
+  "vue",
+  "php",
+  "astro",
+}
+
 return {
-  "luckasRanarison/tailwind-tools.nvim",
-  ft = {
-    "html",
-    "svelte",
-    "css",
-    "javascript",
-    "typescript",
-    "javascriptreact",
-    "typescriptreact",
-    "vue",
-    "php",
-    "astro",
+  {
+    -- tailwind-tools.lua
+    "luckasRanarison/tailwind-tools.nvim",
+    ft = web_filetypes,
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {}, -- your configuration
   },
-  name = "tailwind-tools",
-  build = ":UpdateRemotePlugins",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-telescope/telescope.nvim", -- optional
-    "neovim/nvim-lspconfig", -- optional
+  {
+    "windwp/nvim-ts-autotag",
+    ft = web_filetypes,
+    opts = {},
   },
-  opts = {}, -- your configuration
 }
