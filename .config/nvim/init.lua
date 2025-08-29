5e61b6b334cc30545b0877f13b6c8821e6d971cf
Agsayan <agsayan0@gmail.com>
2025-08-29 09:46:07 +0200

    refactor: redir now loads before my plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@ vim.loader.enable() -- new experimental lua-loader that byte-compiles and caches
 require("config.options")
 require("config.keymaps")
 require("config.dotenv")
-require("config.lazy") -- load my plugins
 require("config.redir")
+require("config.lazy") -- load my plugins
 require("config.autocmds")
 require("config.user_cmds")
