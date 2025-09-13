d3fc99064b0f1a2ed66d5ea71d18c9ada95b7875
Agsayan <agsayan0@gmail.com>
2025-09-13 22:54:06 +0200

    fix(plugin): removed unused ts-error-translator.nvim plugin

--- .config/nvim/lua/plug/typescript.lua
+++ .config/nvim/lua/plug/typescript.lua
@@ -5,10 +5,4 @@ return {
     opts = true,
     ft = { "typescript", "javascript" },
   },
-
-  {
-    "dmmulroy/ts-error-translator.nvim",
-    ft = { "typescript", "javascript" },
-    config = function() require("ts-error-translator").setup() end,
-  },
 }
