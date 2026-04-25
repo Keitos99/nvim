cf50bc963fa0369f468f7b6156192da1a76ec137
Agsayan <agsayan0@gmail.com>
2026-04-25 14:13:06 +0200

    Removed unnecessary configurations for lualine.nvim

--- .config/nvim/lua/plug/ui.lua
+++ .config/nvim/lua/plug/ui.lua
@@ -91,44 +91,24 @@ return {
     opts = {
       options = {
         icons_enabled = true,
-        theme = "auto",
         disabled_filetypes = {
-          statusline = {},
           winbar = {
             "dap-view",
             "dap-repl",
             "dap-view-term",
           },
         },
-        ignore_focus = {},
-        always_divide_middle = true,
         globalstatus = true,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        },
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_b = { "branch", "diagnostics" },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_z = { "location" },
       },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = {},
     },
   },
   {
-    -- NOTE: the tohtml command does not work if this plugin is enabled
     "lukas-reineke/indent-blankline.nvim",
     event = "BufReadPre",
     main = "ibl",
