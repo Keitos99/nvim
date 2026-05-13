3b00e1dc9aa4137615ca817498897ca510ec9998
Agsayan <agsayan0@gmail.com>
2026-05-13 16:10:20 +0200

    Removed the CVS Plugin

--- .config/nvim/lua/plug/init.lua
+++ .config/nvim/lua/plug/init.lua
@@ -13,27 +13,4 @@ return {
       require("e4glide").setup()
     end,
   },
-  {
-    dir = vim.fn.expand("~") .. "/dev/personal/cvs.nvim",
-    cmd = {
-      "CVSCompare",
-      "CVSReplace",
-      "CVSBlame",
-      "CVSChose",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      local Helper = require("config.helper")
-
-      require("cvs").setup({
-        find_root = Helper.find_root,
-        is_available = function()
-          local is_vpn_connected = vim.fn.system("ip route | grep -q 10.8.0.1; echo $?") ~= 1
-          return is_vpn_connected
-        end,
-      })
-    end,
-  },
 }
